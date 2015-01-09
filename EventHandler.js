5acdb5d1337fd023a6d4773655e5e572f65a025c
Yehonal <hw.2@hotmail.it>
2015-01-09 11:11:19 +0100

    Fixed tools compiling

---
+++ EventHandler.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2007 - 2014 Hyperweb2 All rights reserved.
+ * GNU General Public License version 3; see www.hyperweb2.com/terms/
+ */
+
+'use strict';
+
+hw2.define([
+    "hw2!PATH_JS_LIB:browser/event/include.js"
+], function () {
+    var $ = this;
+    $.Browser.EventHandler = $.Class({members: [
+            {
+                attributes: "public static",
+                name: "onBodyLoad",
+                val: function (handler) {
+                    if (document.readyState === "complete") {
+                        handler();
+                        return;
+                    }
+
+                    if (window.addEventListener) {
+                        window.addEventListener("load", handler, false);
+                    }
+                    else if (window.attachEvent) {
+                        window.attachEvent("onload", handler);
+                    }
+                }
+            }
+        ]}
+    );
+});
